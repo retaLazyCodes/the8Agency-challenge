d39a4571605e401082e3dd7af0bb8372ed3d3a10
Brian Retamar <leo1632@hotmail.com>
2021-12-08 06:51:58 -0300

    Added components

---
+++ front/src/components/Description/index.js
@@ -0,0 +1,25 @@
+export const Description = () => {
+    return (
+        <div className='description'>
+            <p className='description__text'>
+                Te invitan este webinar, donde trataremos la temática de cómo humanizar la experiencia del cliente de Banca y
+                Seguros en el nuevo entorno digital.
+            </p>
+
+            <p className='description__text'>
+                Además podremos conocer las estrategias que aplicó LOREM para generar una experiencia memorable para sus
+                clientes, mientras se convertía en el gran Unicornio de LATAM.
+            </p>
+
+            <p className='description__text'>Escucha de primera mano la voz de nuestros especialistas:</p>
+
+
+            <p className='description__text'>
+                Participa e inspírate para innovar y mejorar la interacción entre clientes y marcas con historias de éxito de
+                empresas del ámbito financiero en América Latina.
+            </p>
+
+            <p className='description__text'>¡Te esperamos!</p>
+        </div>
+    );
+};
---
+++ front/src/components/MainContent/index.js
@@ -0,0 +1,11 @@
+import { Description } from "components/Description";
+
+import './styles.css';
+
+export const MainContent = () => {
+    return (
+        <div className='main-content'>
+            <Description />
+        </div>
+    );
+};
