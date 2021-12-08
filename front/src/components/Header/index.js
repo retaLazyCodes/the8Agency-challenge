f2a954e63098f17e11cadf4b5a45969c4b5d549c
Brian Retamar <leo1632@hotmail.com>
2021-12-08 07:48:47 -0300

    Made Header reusable

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -1,13 +1,21 @@
 import headerBg from "assets/header-background.svg";
 import './styles.css';
 
-export const Header = () => {
+export const Header = ({ message }) => {
     return (
         <div className='header' style={{ background: `#ffd6a8 url(${headerBg}) center no-repeat` }}>
-            <span className='header__category'>Webinar</span>
-            <h2 className='header__title'>El reto de humanizar el CX financiero en 2021.</h2>
-            <h2 className='header__subtitle'>La experiencia de un Unicornio de Latam</h2>
-            <p className='header__text'>Miércoles 16 de diciembre | 17 HS (horario de Quito)</p>
+            {message ?
+                <div className='header'>
+                    <span className='header__category'>Webinar</span>
+                    <h2 className='header__subtitle'>{message}</h2>
+                </div> :
+                <div className='header'>
+                    <span className='header__category'>Webinar</span>
+                    <h2 className='header__title'>El reto de humanizar el CX financiero en 2021.</h2>
+                    <h2 className='header__subtitle'>La experiencia de un Unicornio de Latam</h2>
+                    <p className='header__text'>Miércoles 16 de diciembre | 17 HS (horario de Quito)</p>
+                </div>
+            }
         </div>
     );
 };
