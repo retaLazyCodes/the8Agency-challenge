bcca6c3aafc46a0f45ffca97c540b07c15de98c7
Brian Retamar <leo1632@hotmail.com>
2021-12-08 02:13:19 -0300

    compiled sass

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import { Header } from 'components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mi app de eventos</h1>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
