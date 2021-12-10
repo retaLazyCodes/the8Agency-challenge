d56265c7bb829b97f5b091094d3a5e0973461e62
Brian Retamar <leo1632@hotmail.com>
2021-12-10 15:36:31 -0300

    styled SocialLinks

--- front/src/components/SocialLinks/index.js
+++ front/src/components/SocialLinks/index.js
@@ -7,41 +7,43 @@ import {
     FaYoutube
 } from 'react-icons/fa';
 
+import './styles.css'
+
 export const SocialLinks = () => {
     return (
         <ul className='social-media'>
             <li>
-                <a className='social-media__link' href='https://twitter.com' target='_blank' rel='noopener noreferrer'>
+                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
                     <FaTwitter size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
+                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
                     <FaFacebookF size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='https://linkedin.com' target='_blank' rel='noopener noreferrer'>
+                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
                     <FaLinkedinIn size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='https://youtube.com' target='_blank' rel='noopener noreferrer'>
+                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
                     <FaYoutube size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
                     <FaInstagram size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='https://snapshat.com' target='_blank' rel='noopener noreferrer'>
+                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
                     <FaSnapchatGhost size={24} color='white' />
                 </a>
             </li>
