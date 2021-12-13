36cb561ba9d0378d9715a0ceb62369dcb23cc66b
Brian Retamar <leo1632@hotmail.com>
2021-12-13 01:05:16 -0300

    listed attendees

--- front/src/components/AttendeesList/index.js
+++ front/src/components/AttendeesList/index.js
@@ -1,3 +1,35 @@
+import MaterialTable from 'material-table'
+import { useSelector } from 'react-redux';
+
 export const AttendeesList = () => {
-    return <h1>AttendeesList</h1>
+    const attendees = useSelector(state => state)
+
+    //configuración de columnas de material-table
+    const columns = [
+        { title: "Nombre", field: "name" },
+        { title: "Apellido", field: "lastname" },
+        { title: 'Correo electrónico', field: 'email' },
+        { title: 'País', field: 'country' },
+        { title: 'Teléfono', field: 'phone' },
+        { title: 'Puesto de trabajo', field: 'job' },
+    ];
+
+    // objeto para configurar el idioma de los mensajes por defecto en material-table
+    const localization = {
+        toolbar: { searchPlaceholder: "Buscar" },
+    };
+
+    return (
+        <div style={{ maxWidth: '90%', margin: 'auto' }}>
+            <div>
+                <MaterialTable
+                    title="Lista de Asistentes"
+                    columns={columns}
+                    data={attendees}
+                    localization={localization}
+                />
+            </div>
+        </div>
+    )
+
 }
--- front/src/components/SocialLinks/index.js
+++ front/src/components/SocialLinks/index.js
@@ -13,37 +13,37 @@ export const SocialLinks = () => {
     return (
         <ul className='social-media'>
             <li>
-                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
+                <a className='social-media__link' href='#' rel='noopener noreferrer'>
                     <FaTwitter size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
+                <a className='social-media__link' href='#' rel='noopener noreferrer'>
                     <FaFacebookF size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
+                <a className='social-media__link' href='#' rel='noopener noreferrer'>
                     <FaLinkedinIn size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
+                <a className='social-media__link' href='#' rel='noopener noreferrer'>
                     <FaYoutube size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
+                <a className='social-media__link' href='#' rel='noopener noreferrer'>
                     <FaInstagram size={24} color='white' />
                 </a>
             </li>
 
             <li>
-                <a className='social-media__link' href='!#' rel='noopener noreferrer'>
+                <a className='social-media__link' href='#' rel='noopener noreferrer'>
                     <FaSnapchatGhost size={24} color='white' />
                 </a>
             </li>
